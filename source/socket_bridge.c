e4bc7c3b94f42f9a6b1d1b9c634e946a9570ebdf
ross-shk <ross_shk@outlook.com>
2026-06-30 13:03:32 +0300

    basic heap interaction fix

--- source/socket_bridge.c
+++ source/socket_bridge.c
@@ -49,7 +49,7 @@ int bind_to_port(int socket_fd, int port, int af, int inaddr) {
 }
 
 int resolve_hostname(char *hostname, int family,
-  char *out_ip, int out_len) {
+                     char *out_ip, int out_len) {
   struct addrinfo hints = {0};
   struct addrinfo *result;
   int ret;
@@ -58,21 +58,8 @@ int resolve_hostname(char *hostname, int family,
   hints.ai_socktype = SOCK_STREAM;
 
   ret = getaddrinfo(hostname, NULL, &hints, &result);
-  if (ret != 0) {
-    switch (ret) {
-      case EAI_AGAIN:    errno = EAGAIN; break;
-      case EAI_BADFLAGS: errno = EINVAL; break;
-      case EAI_FAIL:     errno = EHOSTUNREACH; break;
-      case EAI_FAMILY:   errno = EAFNOSUPPORT; break;
-      case EAI_MEMORY:   errno = ENOMEM; break;
-      case EAI_NONAME:   errno = EHOSTUNREACH; break;
-      case EAI_SERVICE:  errno = EINVAL; break;
-      case EAI_SOCKTYPE: errno = EINVAL; break;
-      case EAI_SYSTEM:   break;
-      default:           errno = EINVAL; break;
-    }
-    return -1;
-  }
+  if (ret != 0)
+      return -1;
 
   if (result->ai_family == AF_INET) {
     struct sockaddr_in *a = (struct sockaddr_in *)result->ai_addr;
@@ -85,7 +72,7 @@ int resolve_hostname(char *hostname, int family,
     errno = EAFNOSUPPORT;
     return -1;
   }
-
+  
   freeaddrinfo(result);
   return 0;
 }
